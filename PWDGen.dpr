60074ec91d55815351828f5a1123cc810d623046
superbot-coder <knst.spb@gmail.com>
2020-06-12 15:31:34 +0800

    v. 1.0.0.0

---
+++ PWDGen.dpr
@@ -0,0 +1,14 @@
+program PWDGen;
+
+uses
+  Vcl.Forms,
+  UMain in 'UMain.pas' {Main};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMain, Main);
+  Application.Run;
+end.
